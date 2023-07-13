e3e903b7fb500fbe4dc9a4349d280c3b31f75454
Islam <iesdanzade@gmail.com>
2023-07-13 15:21:18 +0400

    Project initially added

---
+++ delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace delegates
+{
+    internal class Program
+    {
+        delegate void mydelegate(int say1, int say2);
+        static void Main(string[] args)
+        {
+            mydelegate menimdelegate = new mydelegate(topla);
+            menimdelegate.Invoke(4, 5);
+            Console.WriteLine("Hello, World!");
+        }
+        static void topla(int eded1,int eded2) 
+        {
+            Console.WriteLine(eded1+eded2);
+        }
+    }
+}
